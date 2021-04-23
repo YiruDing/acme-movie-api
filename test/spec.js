0b2385224f8252e5de9b98dd63ef42fdf79fe71a
Yi-Ru Ding <dingyr0925@gmail.com>
2021-04-22 20:12:55 -0400

    first commit

---
+++ test/spec.js
@@ -0,0 +1,10 @@
+const {expect} = require('chai');
+const app = require('supertest')(require('../app'));
+describe('Routes',()=>{
+    describe('GET /',()=>{
+        it('show information about the api',async()=>{ 
+            const res = await app.get('/');
+            expect(res.status).to.equal(200);
+        })
+    })
+})
